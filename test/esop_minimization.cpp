d9156dd2245078596ab1929c3e604e054b2ebd95
msoeken <msoeken@informatik.uni-bremen.de>
2014-05-25 14:00:23 +0200

    ESOP minimization.

--- test/esop_minimization.cpp
+++ test/esop_minimization.cpp
@@ -1,9 +1,12 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE esop_minimization
 
+#include <boost/format.hpp>
 #include <boost/test/unit_test.hpp>
 
 #include <classical/optimization/esop_minimization.hpp>
+
+#define COMPARE_WITH_EXORCISM 1
 
 BOOST_AUTO_TEST_CASE(simple)
 {
@@ -17,14 +20,18 @@ BOOST_AUTO_TEST_CASE(simple)
 
   properties::ptr statistics( new properties() );
 
-  if ( master_test_suite().argc == 2u )
-  {
-    esop_minimization( master_test_suite().argv[1], settings, statistics );
-  }
-  else
-  {
-    esop_minimization( "../test/example.pla", settings, statistics );
-  }
+  std::string filename = ( master_test_suite().argc == 2u ) ? master_test_suite().argv[1] : "../test/example.pla";
+
+  esop_minimization( filename, settings, statistics );
+
+#if COMPARE_WITH_EXORCISM
+  system( boost::str( boost::format( "(exorcism %s; echo) > /dev/null" ) % filename ).c_str() );
+
+  std::cout << "EXORCISM cubes:     "; std::cout.flush();
+  system( boost::str( boost::format( "cat %sesop | grep \"Final\" | awk '{print $6}' ") % filename.substr( 0u, filename.size() - 3u ) ).c_str() );
+  std::cout << "EXORCISM literals:  "; std::cout.flush();
+  system( boost::str( boost::format( "cat %sesop | grep \"Final\" | awk '{print $9}' ") % filename.substr( 0u, filename.size() - 3u ) ).c_str() );
+#endif
 
   std::cout << "Number of cubes:    " << statistics->get<unsigned>( "cube_count" ) << std::endl
             << "Number of literals: " << statistics->get<unsigned>( "literal_count" ) << std::endl
