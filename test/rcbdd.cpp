0d58216992f41d7648ba225fc74ad476b94857d7
msoeken <msoeken@informatik.uni-bremen.de>
2014-06-02 23:28:33 +0200

    RCBDD synthesis.

--- test/rcbdd.cpp
+++ test/rcbdd.cpp
@@ -12,14 +12,16 @@
 #include <core/rcbdd.hpp>
 #include <core/truth_table.hpp>
 #include <core/functions/extend_pla.hpp>
+#include <core/functions/circuit_to_truth_table.hpp>
 #include <core/io/read_pla.hpp>
 #include <core/io/write_pla.hpp>
 #include <core/io/write_realization.hpp>
+#include <core/utils/foreach_function.hpp>
 #include <core/utils/timer.hpp>
+#include <algorithms/simulation/simple_simulation.hpp>
 #include <algorithms/synthesis/embed_pla.hpp>
 #include <algorithms/synthesis/rcbdd_synthesis.hpp>
-
-#include <core/utils/foreach_function.hpp>
+#include <algorithms/synthesis/young_subgroup_synthesis.hpp>
 
 using namespace boost::assign;
 
@@ -29,13 +31,15 @@ BOOST_AUTO_TEST_CASE(simple)
 
   std::vector<std::string> whitelist;
   //whitelist += "sym6_63","con1_136";//,"bw_116";
-  whitelist += "sym6_63","urf2_73","con1_136","hwb9_65","urf1_72","urf5_76","sym9_71","urf3_75","rd84_70","sym10_207","urf4_89";
+  //whitelist += "sym6_63","urf2_73","con1_136","hwb9_65","urf1_72","urf5_76","sym9_71","urf3_75","rd84_70","sym10_207","urf4_89";
+  whitelist += "sym6_63";
   //whitelist += "sym6_63","urf2_73","con1_136","hwb9_65","urf1_72","urf5_76","sym9_71","urf3_75","rd84_70","sym10_207","urf4_89","adr4_93","cycle_10_2_61","clip_124","dc2_143","misex1_178","co14_135","urf6_77","dk27_146","t481_208","5xp1_90","C7552_119","apla_107","bw_116";
   // extra alu1_94
   foreach_function_with_whitelist( whitelist, []( const boost::filesystem::path& path ) {
     circuit circ;
     rcbdd cf;
     binary_truth_table pla, extended;
+    binary_truth_table spec;
 
     std::cout << "Process: " << path.relative_path().string() << std::endl;
     std::string name = path.stem().string();
@@ -86,6 +90,26 @@ BOOST_AUTO_TEST_CASE(simple)
     }
 
     write_realization( circ, boost::str( boost::format( "/tmp/%s.real" ) % name ) );
+
+    {
+      print_timer pt( std::cout );
+      timer<print_timer> timer( pt );
+      std::cout << " - simul:  ";
+
+      circuit_to_truth_table( circ, spec, simple_simulation_func() );
+    }
+
+    {
+      circuit ys_circ;
+
+      print_timer pt( std::cout);
+      timer<print_timer> timer( pt );
+      std::cout << " - vshape: ";
+
+      young_subgroup_synthesis( ys_circ, spec );
+
+      write_realization( ys_circ, boost::str( boost::format( "/tmp/%s-ys.real" ) % name ) );
+    }
   });
 }
 
