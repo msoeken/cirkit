c7a0ac0bd02972af8d6af409ab262df0e41107a2
msoeken <msoeken@informatik.uni-bremen.de>
2014-06-02 18:40:47 +0200

    Young subgroup synthesis.

--- src/algorithms/synthesis/young_subgroup_synthesis.cpp
+++ src/algorithms/synthesis/young_subgroup_synthesis.cpp
@@ -34,6 +34,8 @@
 #include <core/io/write_pla.hpp>
 #include <core/utils/timer.hpp>
 
+#include <classical/optimization/esop_minimization.hpp>
+
 #include <cuddObj.hh>
 
 using namespace boost::assign;
@@ -55,59 +57,32 @@ public:
     }
   }
 
-  void add_gate_for_cube( circuit& gatecirc, unsigned target, const std::string& str )
+  void add_gate_for_cube( circuit& gatecirc, unsigned target, const cube_t& cube )
   {
     gate::control_container controls;
 
-    for ( unsigned i = 0u; i < str.size(); ++i )
+    for ( unsigned i = 0u; i < circ.lines(); ++i )
     {
-      if ( str[i] == '-' ) continue;
-      unsigned c = ( i >= target ) ? i + 1 : i;
-      controls += make_var( c, str[i] == '1' );
+      if ( !cube.second[i] ) continue;
+      controls += make_var( i, cube.first[i] );
     }
 
     append_toffoli( gatecirc, controls, target );
   }
 
-  void synthesis_gate( binary_truth_table const& spec, unsigned target, unsigned& start )
+  void synthesis_gate( BDD control_function, unsigned target, unsigned& start )
   {
-    write_pla( spec, "v.pla" );
-    system((char *)"exorcism -q 3 v.pla >temp.dat 2>temp.dat");
+    circuit gatecirc( circ.lines() );
 
-    std::ifstream fin("v.esop");
-    std::string str;
-    circuit cir(circ.lines());
-    if (fin >> str)
-    {
-      while (str != "esop")
-      {
-        fin >> str;
-      }
-      while (str != ".e")
-      {
-        fin >> str;
-        if (str != ".e")
-        {
-          add_gate_for_cube( cir, target, str );
-          fin >> str;
-        }
-        else
-        {
-          break;
-        }
-      }
-      fin.close();
+    properties::ptr settings( new properties() );
+    settings->set( "on_cube", cube_function_t( [this, &gatecirc, &target]( const cube_t& c ) { add_gate_for_cube( gatecirc, target, c ); } ) );
+    settings->set( "verify", true );
+    esop_minimization( control_function.manager(), control_function.getNode(), settings );
 
-    }
-    else
+    if ( gatecirc.num_gates() )
     {
-      fin.close();
-    }
-
-    if ( cir.num_gates() )
-    {
-      insert_circuit( circ, start, cir );
-      start += cir.num_gates();
+      insert_circuit( circ, start, gatecirc );
+      start += gatecirc.num_gates();
     }
   }
 
@@ -145,100 +120,66 @@ public:
     return i;
   }
 
+  BDD get_control_function( const truth_table_column_t& in,
+                            const truth_table_column_t& out,
+                            unsigned pos )
+  {
+    BDD bdd = cudd.bddZero();
+
+    for ( unsigned i = 0u; i < in.size(); ++i )
+    {
+      if ( in[i][pos] == 0u && out[i][pos] == 1u )
+      {
+        BDD cube = cudd.bddOne();
+
+        for ( unsigned j = 0; j < circ.lines(); ++j )
+        {
+          if ( j == pos ) continue;
+          cube &= in[i][j] == 1 ? cudd.bddVar( j ) : !cudd.bddVar( j );
+        }
+
+        bdd |= cube;
+      }
+    }
+
+    return bdd;
+  }
+
   void add_gates( const truth_table_column_t& vf_in, const truth_table_column_t& vf_out,
                   const truth_table_column_t& vb_in, const truth_table_column_t& vb_out,
                   unsigned pos,
                   unsigned& start)
   {
-    unsigned bw = vf_in[0].size(), back;
-    binary_truth_table spec_front, spec_back;
-    BDD bdd_front = cudd.bddZero(), bdd_back = cudd.bddZero();
-
-    for ( unsigned i = 0u; i < vf_in.size(); ++i )
-    {
-      if ( vf_in[i][pos] == 0u )
-      {
-        if ( vf_out[i][pos] == 1 )
-        {
-          BDD cube = cudd.bddOne();
-
-          for ( unsigned j = 0; j < bw; ++j )
-          {
-            if ( j == pos ) continue;
-            cube &= vf_in[i][j] == 1 ? cudd.bddVar( j ) : !cudd.bddVar( j );
-          }
-
-          bdd_front |= cube;
-        }
-      }
-    }
-
-    for (unsigned i = 0; i < vf_in.size(); ++i) {
-      binary_truth_table::cube_type in_cube, out_cube;
-      if (vf_in[i][pos] == 0) {
-        out_cube.push_back((vf_out[i][pos] == 0) ? false : true);
-        for (unsigned j = 0; j < bw; ++j) {
-          if (j != pos)
-            in_cube.push_back((vf_in[i][j] == 0) ? false : true);
-        }
-        spec_front.add_entry(in_cube, out_cube);
-      }
-      in_cube.clear();
-      out_cube.clear();
-      if (vb_in[i][pos] == 0) {
-        out_cube.push_back((vb_out[i][pos] == 0) ? false : true);
-        for (unsigned j = 0; j < bw; ++j) {
-          if (j != pos)
-            in_cube.push_back((vb_in[i][j] == 0) ? false : true);
-        }
-        spec_back.add_entry(in_cube, out_cube);
-      }
-    }
+    BDD bdd_front = get_control_function( vf_in, vf_out, pos );
+    BDD bdd_back  = get_control_function( vb_in, vb_out, pos );
 
     if ( verbose )
     {
-      std::cout << "SPEC for front:" << std::endl << spec_front << std::endl;
       std::cout << "BDD for front:" << std::endl;
       bdd_front.PrintMinterm();
     }
-    synthesis_gate(spec_front, pos, start);
-    //  std::cout << " Circuit:" << std::endl << circ << std::endl;
-    back = start;
+    synthesis_gate(bdd_front, pos, start);
+    unsigned back = start;
 
     if ( verbose )
     {
-      std::cout << "SPEC for back:" << std::endl << spec_front << std::endl;
+      std::cout << "BDD for back:" << std::endl;
+      bdd_back.PrintMinterm();
     }
-    synthesis_gate(spec_back, pos, back);
-    //  std::cout << " Circuit:" << std::endl << circ << std::endl;
-    //specs.insert(specs.begin() + pos, spec_front);
-    //specs.insert(specs.begin() + pos + 1, spec_back);
+    synthesis_gate(bdd_back, pos, back);
   }
 
   void add_gates( const truth_table_column_t& vf, const truth_table_column_t& vb,
                   unsigned pos, unsigned& start )
   {
-    binary_truth_table spec;
-    unsigned bw = vf[0].size();
-    //print_vec(vf);
-    for (unsigned i = 0; i < vf.size(); ++i) {
-      binary_truth_table::cube_type in_cube, out_cube;
-      if (vf[i][pos] == 0) {
-        out_cube.push_back((vb[i][pos] == 0) ? false : true);
-        for (unsigned j = 0; j < bw; ++j) {
-          if (j != pos)
-            in_cube.push_back((vf[i][j] == 0) ? false : true);
-        }
-        spec.add_entry(in_cube, out_cube);
-      }
-    }
+    BDD bdd = get_control_function( vf, vb, pos );
 
     if ( verbose )
     {
-      std::cout << "SPEC for middle:" << std::endl << spec << std::endl;
+      std::cout << "BDD for middle:" << std::endl;
+      bdd.PrintMinterm();
     }
-    synthesis_gate( spec, pos, start);
-    //  std::cout << " Circuit:" << std::endl << circ << std::endl;
+    synthesis_gate( bdd, pos, start);
   }
 
   void next_step( truth_table_column_t& v_f_in, truth_table_column_t& v_f_out,
