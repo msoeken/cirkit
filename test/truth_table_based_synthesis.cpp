ee4ca764037d3011b5783df1c8b3c91ab137a42b
msoeken <msoeken@informatik.uni-bremen.de>
2014-06-09 23:31:03 +0200

    Truth table based synthesis experiment.

---
+++ test/truth_table_based_synthesis.cpp
@@ -0,0 +1,66 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE truth_table_based_synthesis
+
+#include <boost/test/unit_test.hpp>
+
+#if ADDON_REVLIB
+
+#include <vector>
+
+#include <core/truth_table.hpp>
+#include <core/io/read_pla.hpp>
+#include <core/utils/benchmark_table.hpp>
+#include <core/utils/foreach_function.hpp>
+
+#include <algorithms/synthesis/embed_truth_table.hpp>
+#include <algorithms/synthesis/reed_muller_synthesis.hpp>
+#include <algorithms/synthesis/transformation_based_synthesis.hpp>
+#include <algorithms/synthesis/young_subgroup_synthesis.hpp>
+
+BOOST_AUTO_TEST_CASE(simple)
+{
+  using namespace revkit;
+
+  /* store results */
+  benchmark_table<std::string, unsigned, unsigned, unsigned, unsigned>
+  table( { "Benchmark", "n", "TBS", "RMS", "YSG" } );
+
+  std::vector<std::string> whitelist = { "3_17_6","4_49_7","4gt10_22","4gt11_23","4gt12_24","4gt13_25","4mod5_8","decod24_10","ham3_28","ham7_29","hwb7_15" };
+  foreach_function_with_whitelist( whitelist, [&table]( const boost::filesystem::path& path ) {
+    std::string benchmark = path.stem().string();
+
+    /* output */
+    std::cout << "Processing " << benchmark << "..." << std::endl;
+
+    /* filename */
+    std::string filename = path.relative_path().string();
+
+    /* embedding */
+    binary_truth_table pla, spec;
+    read_pla_settings settings;
+    settings.extend = true;
+    read_pla( pla, path.relative_path().string(), settings );
+    embed_truth_table( spec, pla );
+
+    /* synthesis */
+    circuit circ_tbs, circ_rms, circ_ysg;
+    transformation_based_synthesis( circ_tbs, spec );
+    reed_muller_synthesis( circ_rms, spec );
+    young_subgroup_synthesis( circ_ysg, spec );
+
+    table.add( benchmark, spec.num_inputs(), circ_tbs.num_gates(), circ_rms.num_gates(), circ_ysg.num_gates() );
+  });
+
+  /* generate table */
+  table.print();
+}
+
+#else
+
+BOOST_AUTO_TEST_CASE(simple) {}
+
+#endif
+
+// Local Variables:
+// c-basic-offset: 2
+// End:
