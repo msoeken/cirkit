832f1746c1dabfe50d797aea480e0162318ac68e
msoeken <msoeken@informatik.uni-bremen.de>
2014-06-16 22:46:24 +0200

    Copyright information.

--- src/core/utils/benchmark_table.hpp
+++ src/core/utils/benchmark_table.hpp
@@ -1,4 +1,4 @@
-/* RevKit: A Toolkit for Reversible Circuit Design (www.revkit.org)
+/* CirKit: A circuit toolkit
  * Copyright (C) 2009-2011  The RevKit Developers <revkit@informatik.uni-bremen.de>
  *
  * This program is free software: you can redistribute it and/or modify
