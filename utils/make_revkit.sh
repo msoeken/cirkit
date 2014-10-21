10c11e31826172ea1cda255e7aa48f069167b210
msoeken <msoeken@informatik.uni-bremen.de>
2014-10-21 10:55:24 +0200

    Make RevKit script.

--- utils/make_revkit.sh
+++ utils/make_revkit.sh
@@ -32,10 +32,11 @@ echo -e "\e[1;37m[I]\e[0m Deleting \e[0;32m.gitignore\e[0m file"
 
 rm -Rf revkit-$VERSION/.gitignore
 
-echo -e "\e[1;37m[I]\e[0m Deleting \e[0;32mutils\e[0m and \e[0;32mqa\e[0m directory"
+echo -e "\e[1;37m[I]\e[0m Deleting \e[0;32mutils\e[0m, \e[0;32mqa\e[0m and \e[0;32mnotes\e[0m directory"
 
 rm -Rf revkit-$VERSION/utils
 rm -Rf revkit-$VERSION/qa
+rm -Rf revkit-$VERSION/notes
 
 echo -e "\e[1;37m[I]\e[0m Deleting GUI"
 
