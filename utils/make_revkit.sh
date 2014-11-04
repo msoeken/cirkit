8528ef7318d1f9926318116ff0476c9bc56fe5ec
msoeken <msoeken@informatik.uni-bremen.de>
2014-11-04 08:58:40 +0100

    Make RevKit.

--- utils/make_revkit.sh
+++ utils/make_revkit.sh
@@ -32,16 +32,19 @@ echo -e "\e[1;37m[I]\e[0m Deleting \e[0;32m.gitignore\e[0m and \e[0;32m.travis.y
 
 rm -Rf revkit-$VERSION/.gitignore revkit-$VERSION/.travis.yml
 
-echo -e "\e[1;37m[I]\e[0m Deleting \e[0;32mutils\e[0m, \e[0;32mqa\e[0m and \e[0;32mnotes\e[0m directory"
+echo -e "\e[1;37m[I]\e[0m Deleting \e[0;32mutils/make_revkit.sh\e[0m file"
 
-rm -Rf revkit-$VERSION/utils
+rm -Rf revkit-$VERSION/utils/make_revkit.sh
+
+echo -e "\e[1;37m[I]\e[0m Deleting \e[0;32mqa\e[0m and \e[0;32mnotes\e[0m directory"
+
 rm -Rf revkit-$VERSION/qa
 rm -Rf revkit-$VERSION/notes
 
 echo -e "\e[1;37m[I]\e[0m Deleting GUI"
 
 rm -Rf revkit-$VERSION/gui/programs/gui
-sed -i "25,40d" revkit-$VERSION/gui/CMakeLists.txt
+sed -i "25,41d" revkit-$VERSION/gui/CMakeLists.txt
 
 echo -e "\e[1;37m[I]\e[0m Repacking archive \e[0;32mrevkit-$VERSION.tar.gz\e[0m"
 
