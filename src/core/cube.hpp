2264372864d943da2486b40ad3fcc3a861b3fe6a
msoeken <msoeken@informatik.uni-bremen.de>
2014-11-14 20:27:54 +0100

    Header.

--- src/core/cube.hpp
+++ src/core/cube.hpp
@@ -1,4 +1,5 @@
-/* Copyright (C) 2009-2014  University of Bremen
+/* RevKit (www.revkit.org)
+ * Copyright (C) 2009-2014  University of Bremen
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
