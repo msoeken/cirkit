89b6f29e8a755b37120a76e5db05337411596900
Mathias Soeken <msoeken@informatik.uni-bremen.de>
2015-01-12 17:58:42 +0100

    Package manager.

--- utils/private_packages.py
+++ utils/private_packages.py
@@ -5,3 +5,12 @@ class package_bds:
     fmt         = "tar-gz"
     build       = [ "make all" ]
     install     = [ "make install" ]
+
+# This does not compile yet
+class package_atom:
+    description = "A hackable text editor for the 21st Century"
+    subdir      = "atom"
+    url         = "https://github.com/atom/atom"
+    fmt         = "git"
+    build       = [ "export PYTHON=/usr/bin/python2", "git fetch -p", "git checkout $(git describe --tags `git rev-list --tags --max-count=1`)", "script/build" ]
+    install     = [ "cp -v $TMPDIR/atom-build/Atom/atom %s/atom", "cp -v $TMPDIR/atom-build/Atom/apm %s/apm" ]
