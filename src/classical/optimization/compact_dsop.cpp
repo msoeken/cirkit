dedc0c9809b253ae59f1ded46c905ee2779ef422
Mathias Soeken <msoeken@informatik.uni-bremen.de>
2015-01-17 17:31:48 +0100

    Header.

--- src/classical/optimization/compact_dsop.cpp
+++ src/classical/optimization/compact_dsop.cpp
@@ -1,5 +1,5 @@
 /* CirKit: A circuit toolkit
- * Copyright (C) 2009-2014  University of Bremen
+ * Copyright (C) 2009-2015  University of Bremen
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
