2fcaf31fd9c777f99d92aafb4733f8e216c38068
Finn Haedicke <finn@informatik.uni-bremen.de>
2015-02-13 15:45:47 +0100

    use full path to external libraries
    removed link_directory(ext/lib)
    changed add_ext_library to accept arbitrary many libraries (using ARGN)

--- cmake/AddExtLibrary.cmake
+++ cmake/AddExtLibrary.cmake
@@ -2,10 +2,10 @@
 
 set(ext_libraries "" CACHE STRING "external libraries" FORCE)
 mark_as_advanced(ext_libraries)
-function(add_ext_library library)
+function(add_ext_library)
   if ("${ext_libraries}" STREQUAL "")
-    set(ext_libraries "${library}" CACHE STRING "external libraries" FORCE)
+    set(ext_libraries "${ARGN}" CACHE STRING "external libraries" FORCE)
   else()
-    set(ext_libraries "${ext_libraries};${library}" CACHE STRING "external libraries" FORCE)
+    set(ext_libraries "${ext_libraries};${ARGN}" CACHE STRING "external libraries" FORCE)
   endif()
 endfunction(add_ext_library)
