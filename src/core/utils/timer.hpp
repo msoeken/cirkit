3df8ae49b2e91ad4ee3a0388154557b6894d965e
Mathias Soeken <msoeken@informatik.uni-bremen.de>
2015-02-16 22:15:11 +0100

    Timer.

--- src/core/utils/timer.hpp
+++ src/core/utils/timer.hpp
@@ -42,246 +42,6 @@
 #include <core/properties.hpp>
 
 namespace cirkit {
-  /**
-   * @brief Functor for the timer class which assigns the run-time to a given variable
-   *
-   * Make sure that the variable for the run-time is declared outside of the scope
-   * of time measuring.
-   *
-   * @section sec_example_reference_timer Example
-   *
-   * @code
-   * double runtime;
-   * {
-   *   revkit::reference_timer rt( &runtime );
-   *   revkit::timer<revkit::reference_timer> t( rt );
-   *   // code for which time should be measured
-   * }
-   * @endcode
-   *
-   * @since  1.0
-   */
-  struct reference_timer {
-    /**
-     * Result value of the reference_timer is double,
-     * since it returns the value of the reference value, the run-time
-     * in the operator call. This is only useful, when using the intermediate measurement
-     * in timer.
-     *
-     * @sa timer::operator()()
-     *
-     * @since  1.0
-     */
-    using result_type = double;
-
-    /**
-     * @brief Default constructor
-     *
-     * Available for delayed starting of the timer.
-     *
-     * @since  1.0
-     */
-    reference_timer() {}
-
-    /**
-     * @brief Default constructor
-     *
-     * @param _runtime A pointer referencing to the variable where the run-time should be saved.
-     *
-     * @since  1.0
-     */
-    explicit reference_timer( double* _runtime ) : runtime( _runtime ) {}
-
-    /**
-     * @brief Saves the run-time to the variable
-     *
-     * @param r The run-time
-     *
-     * @since  1.0
-     */
-    double operator()( double r ) const {
-      return ( *runtime = r );
-    }
-
-  private:
-    double* runtime;
-  };
-
-  /**
-   * @brief Measure Method for timer
-   *
-   * This class is only holding one enumeration
-   * used for the timer::set_measure_method method.
-   *
-   * Flags which can be used (also in combination) by the
-   * timer::set_measure_method method.
-   *
-   * @since 1.1
-   */
-  enum class measure_method : unsigned
-  {
-    none = 0, user_time = 1, system_time = 2
-  };
-
-  /**
-   * @brief A generic timer class
-   *
-   * This class measures the time between its constructor and
-   * its deconstructor is called. In the deconstructor a
-   * functor is called which can be assigned in the constructor.
-   *
-   * Because of this design the code for which the run-time has
-   * to be checked has to be enclosed as a block and the timer
-   * needs to be initialized as local variable in the beginning
-   * of the block, so that the deconstructor get called automatically
-   * at the end of the block.
-   *
-   * The Outputter has to implement <b>operator()(double runtime) const</b>.
-   *
-   * Two functors are already implemented in the library:
-   * - print_timer: Prints the run-time to a given output stream
-   * - reference_timer: Assigns the run-time to a variable
-   *
-   * @since  1.0
-   */
-  template<typename Outputter>
-  class timer
-  {
-  public:
-    /**
-     * @brief Constructor which does not start measuring the time
-     *
-     * When delayed starting should be done (using start(const Outputter&)) this
-     * constructor has to be used.
-     *
-     * @since  1.0
-     */
-    timer()
-      : started( false )
-    {
-    }
-
-    /**
-     * @brief Constructor which starts measuring the time
-     *
-     * The \p outputter is copied and called in the
-     * deconstructor with the measured time.
-     *
-     * @param outputter Functor which does something with the measured run-time
-     *
-     * @since  1.0
-     */
-    explicit timer( const Outputter& outputter )
-      : p( outputter ),
-        started( true )
-    {
-      times( &_start );
-    }
-
-    /**
-     * @brief Delayed start
-     *
-     * There might be reasons when the starting of the measurement
-     * should be delayed and not started with the constructor.
-     * For this cases this method can be used.
-     *
-     * @param outputter Functor which does something with the measured run-time
-     *
-     * @since  1.0
-     */
-    void start( const Outputter& outputter )
-    {
-      p = outputter;
-      started = true;
-      times( &_start );
-    }
-
-    /**
-     * @brief Specify the measure method
-     *
-     * Use the flags of the timer class to specify the measure method,
-     * e.g. measure_method::user_time or measure_method::system_time. The flags can also
-     * be merged to capture both and return the sum, i.e. measure_method::user_time | measure_method::system_time.
-     *
-     * @since  1.1
-     */
-    void set_measure_method( unsigned method )
-    {
-      _measure_method = method;
-    }
-
-    /**
-     * @brief Intermediate Call of the timer functor
-     *
-     * This operator also returns the return type of the functor.
-     * This is useful, when using the reference_timer which returns
-     * the value of the reference in the functor call.
-     *
-     * @section sec_example_timer_intermediate Example
-     * In this function the timer is used explicitly without a scope
-     * and the start method as well as the intermediate operator.
-     *
-     * @code
-     * double runtime;
-     * reference_timer rt( &runtime );
-     * timer<reference_timer> t;
-     * // before starting the measurement
-     * t.start( rt );
-     * // calculate a bit
-     * double intermediate_time = t();
-     * // calculate a bit more
-     * intermediate_time = t();
-     * @endcode
-     *
-     * @return The result value of the functor operator (if available)
-     *
-     * @since  1.0
-     */
-    typename Outputter::result_type operator()() const
-    {
-      assert( started );
-
-      struct tms end;
-      times( &end );
-
-      clock_t end_t = 0, start_t = 0;
-
-      if ( _measure_method & static_cast<unsigned>( measure_method::user_time ) )
-      {
-        end_t += end.tms_utime + end.tms_cutime;
-        start_t += _start.tms_utime + _start.tms_cutime;
-      }
-      else if ( _measure_method & static_cast<unsigned>( measure_method::system_time ) )
-      {
-        end_t += end.tms_stime + end.tms_cstime;
-        start_t += _start.tms_stime + _start.tms_cstime;
-      }
-
-      double runtime = ( double( end_t - start_t ) ) / (double)sysconf( _SC_CLK_TCK );
-      return p( runtime );
-    }
-
-    /**
-     * @brief Deconstructor which stops measuring the time
-     *
-     * In this function time is measured again and the functor
-     * is called with the runtime.
-     *
-     * @since  1.0
-     */
-    virtual ~timer() {
-      if ( started )
-      {
-        operator()();
-      }
-    }
-
-  private:
-    struct tms _start;
-    Outputter p; // NOTE: has to be copy
-    bool started;
-    unsigned _measure_method = static_cast<unsigned>( measure_method::user_time );
-  };
 
 /* this will soon replace the old print_timer */
 class new_print_timer : public boost::timer::cpu_timer
@@ -341,6 +101,33 @@ public:
 private:
   const properties::ptr& statistics;
   std::string            key;
+};
+
+class new_reference_timer : public boost::timer::cpu_timer
+{
+public:
+  new_reference_timer( double* runtime )
+    : runtime( runtime )
+  {
+    start();
+  }
+
+  ~new_reference_timer()
+  {
+    if ( !is_stopped() )
+    {
+      stop();
+      if ( runtime )
+      {
+        const double sec = 1000000000.0L;
+        boost::timer::cpu_times const elapsed_times(elapsed());
+        *runtime = ( elapsed_times.system + elapsed_times.user ) / sec;
+      }
+    }
+  }
+
+private:
+  double* runtime = nullptr;
 };
 
 }
