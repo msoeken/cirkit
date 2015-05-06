bff252d6ec63cd6fe6480bb7df038b5be3778140
Mathias Soeken <msoeken@informatik.uni-bremen.de>
2015-05-06 14:24:03 +0200

    Warnings.

--- src/core/io/write_graph_file.hpp
+++ src/core/io/write_graph_file.hpp
@@ -39,8 +39,6 @@ namespace cirkit
 template<typename Graph>
 void write_graph_file( const Graph& g, const std::string& filename, bool verbose = false )
 {
-  using vertex_t = typename boost::graph_traits<Graph>::vertex_descriptor;
-
   if ( verbose )
   {
     std::cout << "[I] write graph file to " << filename << std::endl;
@@ -68,8 +66,6 @@ void write_labeled_graph_file( const Graph& g,
                                EdgeLabelFunc&& edge_label,
                                const std::string& filename, bool verbose = false )
 {
-  using vertex_t = typename boost::graph_traits<Graph>::vertex_descriptor;
-
   if ( verbose )
   {
     std::cout << "[I] write graph file to " << filename << std::endl;
