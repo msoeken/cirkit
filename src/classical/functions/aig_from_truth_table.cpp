aa9ecb4b8685be68c4cf42c909880b874113633e
Mathias Soeken <msoeken@informatik.uni-bremen.de>
2015-10-22 06:14:41 +0200

    AIG from TT.

---
+++ src/classical/functions/aig_from_truth_table.cpp
@@ -0,0 +1,77 @@
+/* CirKit: A circuit toolkit
+ * Copyright (C) 2009-2015  University of Bremen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "aig_from_truth_table.hpp"
+
+#include <vector>
+
+#include <boost/format.hpp>
+
+#include <classical/utils/aig_utils.hpp>
+
+namespace cirkit
+{
+
+/******************************************************************************
+ * Types                                                                      *
+ ******************************************************************************/
+
+/******************************************************************************
+ * Private functions                                                          *
+ ******************************************************************************/
+
+/******************************************************************************
+ * Public functions                                                           *
+ ******************************************************************************/
+
+aig_graph aig_from_truth_table( const tt& t )
+{
+  aig_graph aig;
+  aig_initialize( aig );
+
+  const auto n = tt_num_vars( t );
+
+  for ( auto i = 1u; i <= n; ++i )
+  {
+    aig_create_pi( aig, boost::str( boost::format( "x%d" ) % i ) );
+  }
+
+  const auto& info = aig_info( aig );
+  std::vector<aig_function> minterms;
+
+  foreach_minterm( t, [&]( const boost::dynamic_bitset<>& minterm ) {
+      std::vector<aig_function> cube;
+      for ( auto i = 0u; i < n; ++i )
+      {
+        cube.push_back( {info.inputs[i], !minterm[i]} );
+      }
+
+      minterms.push_back( aig_create_nary_and( aig, cube ) );
+    } );
+
+  aig_create_po( aig, aig_create_nary_or( aig, minterms ), "f" );
+
+  return aig;
+}
+
+}
+
+// Local Variables:
+// c-basic-offset: 2
+// eval: (c-set-offset 'substatement-open 0)
+// eval: (c-set-offset 'innamespace 0)
+// End:
---
+++ src/classical/functions/aig_from_truth_table.hpp
@@ -0,0 +1,46 @@
+/* CirKit: A circuit toolkit
+ * Copyright (C) 2009-2015  University of Bremen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file aig_from_truth_table.hpp
+ *
+ * @brief Create AIG from truth table
+ *
+ * @author Mathias Soeken
+ * @since  2.3
+ */
+
+#ifndef AIG_FROM_TRUTH_TABLE_HPP
+#define AIG_FROM_TRUTH_TABLE_HPP
+
+#include <classical/aig.hpp>
+#include <classical/utils/truth_table_utils.hpp>
+
+namespace cirkit
+{
+
+aig_graph aig_from_truth_table( const tt& t );
+
+}
+
+#endif
+
+// Local Variables:
+// c-basic-offset: 2
+// eval: (c-set-offset 'substatement-open 0)
+// eval: (c-set-offset 'innamespace 0)
+// End:
