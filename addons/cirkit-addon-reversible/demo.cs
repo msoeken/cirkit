a176864b692bbaf8964e3ed9cf5de6258c8b605b
Mathias Soeken <mathias.soeken@gmail.com>
2016-02-14 18:16:46 +0100

    RevKit demo.

--- addons/cirkit-addon-reversible/demo.cs
+++ addons/cirkit-addon-reversible/demo.cs
@@ -1,18 +1,39 @@
-#--------------------------------------#
-# Synthesize circuit from permutation  #
-#--------------------------------------#
-# 1. TT from permutation               #
-# 2. Print truth table                 #
-# 3. Transf.-based synthesis           #
-# 4. Print circuit                     #
-# 5. Clear truth table and circuit     #
-#--------------------------------------#
+#----------------------------------#
+# Transformation based synthesis   #
+#----------------------------------#
+# 1. TT from permutation           #
+# 2. Print truth table             #
+# 3. Transf.-based synthesis       #
+# 4. Print circuit                 #
+# 5. Clear truth table and circuit #
+#----------------------------------#
 
 spec -p "0 1 2 4 3 5 6 7"
 print -s
 tbs
 print -c
 store --clear -s -c
+
+
+#------------------------------------------#
+# Symbolic transformation based synthesis  #
+#------------------------------------------#
+# 1. Read BDD from PLA                     #
+# 2. Embed BDD as RCBDD                    #
+# 3. Synthesize circuit from RCBDD         #
+# 4. Print circuits                        #
+# 5. Show circuit statistics               #
+# 6. Show BDD, RCBDD, and circuit in store #
+# 7. Clear BDD, RCBDD, and circuit         #
+#------------------------------------------#
+
+read_pla ext/functions/cycle10_2_61.pla
+embed -b
+tbs -b
+print -c
+ps -c
+store -b -r -c
+store --clear -b -r -c
 
 
 #--------------------------------------#
