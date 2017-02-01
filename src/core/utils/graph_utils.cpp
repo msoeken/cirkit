33f6ac2abdad5573eacb5ed90b609fe2ffc832e7
Mathias Soeken <mathias.soeken@epfl.ch>
2017-02-01 09:30:08 +0100

    Graph utils.

---
+++ src/core/utils/graph_utils.cpp
@@ -0,0 +1,60 @@
+/* CirKit: A circuit toolkit
+ * Copyright (C) 2009-2015  University of Bremen
+ * Copyright (C) 2015-2016  EPFL
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "graph_utils.hpp"
+
+namespace cirkit
+{
+
+graph_t<> line_graph( unsigned order )
+{
+  graph_t<> g;
+  auto last = add_vertex( g );
+
+  for ( auto i = 1; i < order; ++i )
+  {
+    auto next = add_vertex( g );
+    add_edge( last, next, g );
+    last = next;
+  }
+
+  return g;
+}
+
+graph_t<> ring_graph( unsigned order )
+{
+  auto g = line_graph( order );
+  add_edge( order - 1, 0, g );
+  return g;
+}
+
+}
+
+// Local Variables:
+// c-basic-offset: 2
+// eval: (c-set-offset 'substatement-open 0)
+// eval: (c-set-offset 'innamespace 0)
+// End:
