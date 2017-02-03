7b91cb56b85702bca62d5dc1ab47db4f1a7d753f
Mathias Soeken <mathias.soeken@epfl.ch>
2017-02-03 11:18:26 +0100

    Pauli gates.

---
+++ addons/cirkit-addon-reversible/src/reversible/pauli_tags.cpp
@@ -0,0 +1,67 @@
+/* CirKit: A circuit toolkit
+ * Copyright (C) 2009-2015  University of Bremen
+ * Copyright (C) 2015-2016  EPFL
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "pauli_tags.hpp"
+
+#include <reversible/target_tags.hpp>
+
+namespace cirkit
+{
+
+bool is_pauli( const gate& g )
+{
+  return is_type<pauli_tag>( g.type() );
+}
+
+gate& create_pauli( gate& g, unsigned target, pauli_axis axis, unsigned root )
+{
+  g.add_target( target );
+  g.set_type( pauli_tag( axis, root ) );
+  return g;
+}
+
+gate& append_pauli( circuit& circ, unsigned target, pauli_axis axis, unsigned root )
+{
+  return create_pauli( circ.append_gate(), target, axis, root );
+}
+
+gate& prepend_pauli( circuit& circ, unsigned target, pauli_axis axis, unsigned root  )
+{
+  return create_pauli( circ.prepend_gate(), target, axis, root );
+}
+
+gate& insert_pauli( circuit& circ, unsigned n, unsigned target, pauli_axis axis, unsigned root )
+{
+  return create_pauli( circ.insert_gate( n ), target, axis, root );
+}
+
+}
+
+// Local Variables:
+// c-basic-offset: 2
+// eval: (c-set-offset 'substatement-open 0)
+// eval: (c-set-offset 'innamespace 0)
+// End:
---
+++ addons/cirkit-addon-reversible/src/reversible/pauli_tags.hpp
@@ -0,0 +1,84 @@
+/* CirKit: A circuit toolkit
+ * Copyright (C) 2009-2015  University of Bremen
+ * Copyright (C) 2015-2016  EPFL
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/**
+ * @file pauli_tags.hpp
+ *
+ * @brief Pauli(-root) gates
+ *
+ * @author Mathias Soeken
+ * @since  2.3
+ */
+
+#ifndef PAULI_TAGS_HPP
+#define PAULI_TAGS_HPP
+
+#include <reversible/circuit.hpp>
+#include <reversible/gate.hpp>
+
+namespace cirkit
+{
+
+enum class pauli_axis { X, Y, Z };
+
+/**
+ * @brief Target tag for Pauli gates
+ */
+struct pauli_tag
+{
+  pauli_tag() {}
+
+  pauli_tag( pauli_axis axis, unsigned root = 1u )
+    : axis( axis ), root( root )
+  {
+  }
+
+  /**
+   * @brief Axis of Pauli gate
+   */
+  pauli_axis axis = pauli_axis::X;
+
+  /**
+   * @brief root, i.e., U^{1/root}
+   */
+  unsigned root = 1u;
+};
+
+bool is_pauli( const gate& g );
+
+gate& append_pauli( circuit& circ, unsigned target, pauli_axis axis, unsigned root = 1u );
+gate& prepend_pauli( circuit& circ, unsigned target, pauli_axis axis, unsigned root = 1u );
+gate& insert_pauli( circuit& circ, unsigned n, unsigned target, pauli_axis axis, unsigned root = 1u );
+
+}
+
+#endif
+
+// Local Variables:
+// c-basic-offset: 2
+// eval: (c-set-offset 'substatement-open 0)
+// eval: (c-set-offset 'innamespace 0)
+// End:
