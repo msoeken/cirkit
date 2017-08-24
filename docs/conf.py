683268743e1a8784fb501df2d2f8d5eaea44c849
Mathias Soeken <mathias.soeken@epfl.ch>
2017-08-24 12:06:42 +0200

    Docs.

--- docs/conf.py
+++ docs/conf.py
@@ -98,11 +98,11 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_context = {
-    'css_files': [
-        '_static/theme_overrides.css', # overrides wide tables in RTD theme
-    ],
-}
+#html_context = {
+#    'css_files': [
+#        '_static/theme_overrides.css', # overrides wide tables in RTD theme
+#    ],
+#}
 
 # -- Options for HTMLHelp output ------------------------------------------
 
