626311a871906472e3496c2bf95cfc794d7cf99f
Mathias Soeken <mathias.soeken@epfl.ch>
2017-12-19 20:31:16 +0100

    Exact decomposition.

---
+++ src/classical/xmg/xmg_exact_decomp.hpp
@@ -0,0 +1,355 @@
+/* CirKit: A circuit toolkit
+ * Copyright (C) 2009-2015  University of Bremen
+ * Copyright (C) 2015-2017  EPFL
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/**
+ * @file xmg_exact_decomp.hpp
+ *
+ * @brief Exact MAJ decomposition
+ *
+ * @author Mathias Soeken
+ * @author Eleonora Testa
+ * @since  2.4
+ */
+
+#ifndef XMG_EXACT_DECOMP_HPP
+#define XMG_EXACT_DECOMP_HPP
+
+#include <array>
+#include <cstdint>
+#include <iostream>
+#include <vector>
+
+#include <classical/abc/abc_api.hpp>
+#include <sat/glucose/AbcGlucose.h>
+#include <fmt/format.h>
+
+namespace cirkit
+{
+
+namespace detail
+{
+
+template<uint32_t NumVars, uint32_t NumGates>
+struct xmg_exact_decomposition_impl
+{
+public:
+  xmg_exact_decomposition_impl()
+  {
+    for ( auto& a : select )
+    {
+      for ( auto& a2 : a )
+      {
+        a2.fill( 0 );
+      }
+    }
+
+    add_structure_variables();
+
+    solver = abc::bmcg_sat_solver_start();
+    abc::bmcg_sat_solver_set_nvars( solver, var_index );
+  }
+
+  ~xmg_exact_decomposition_impl()
+  {
+    abc::bmcg_sat_solver_stop( solver );
+  }
+
+  bool run()
+  {
+    if ( !add_main_cnf() )
+    {
+      return false;
+    }
+
+    // add value CNF for each interesting minterm
+    for ( uint32_t i = 0; i < ( uint32_t( 1 ) << NumVars ); ++i )
+    {
+      auto cnt = __builtin_popcount( i );
+
+      switch ( cnt )
+      {
+      default:
+        break;
+      case NumVars / 2 - 1:
+        if ( !add_minterm( i, 0 ) )
+        {
+          return false;
+        }
+        break;
+      case NumVars / 2 + 1:
+        if ( !add_minterm( i, 1 ) )
+        {
+          return false;
+        }
+        break;
+      case NumVars / 2:
+        if ( !add_minterm( i, 2 ) )
+        {
+          return false;
+        }
+        break;
+      }
+    }
+
+    if ( abc::bmcg_sat_solver_solve( solver, nullptr, 0 ) == GLUCOSE_SAT )
+    {
+      return true;
+    }
+    return true;
+  }
+
+  void print_solution( std::ostream& os = std::cout )
+  {
+    for ( auto i = 0; i < NumGates; ++i )
+    {
+      os << fmt::format( "n{} = <", i + NumVars );
+
+      for ( auto k = 0; k < 3; ++k )
+      {
+        for ( auto j = 0; j < NumVars + i; ++j )
+        {
+          if ( select[i][k][j] && abc::bmcg_sat_solver_read_cex_varvalue( solver, select[i][k][j] ) )
+          {
+            os << fmt::format( "{}{}", j < NumVars ? 'x' : 'n', j );
+          }
+        }
+      }
+
+      os << ">" << std::endl;
+    }
+  }
+
+private:
+  void add_structure_variables()
+  {
+    for ( auto i = 0; i < NumGates; ++i )
+    {
+      for ( auto k = 0; k < 3; ++k )
+      {
+        /* TODO remove some select variables */
+        for ( auto j = 0; j < NumVars + i; ++j )
+        {
+          select[i][k][j] = var_index;
+          if ( j >= NumVars )
+          {
+            output_lits[j - NumVars].push_back( abc::Abc_Var2Lit( var_index, 0 ) );
+          }
+          var_index++;
+        }
+      }
+    }
+  }
+
+  bool add_main_cnf()
+  {
+    /* every node has three children */
+    for ( auto i = 0; i < NumGates; ++i )
+    {
+      for ( auto k = 0; k < 3; ++k )
+      {
+        std::vector<int> lits;
+
+        for ( auto j = 0; j < NumVars + i; ++j )
+        {
+          if ( !select[i][k][j] )
+            continue;
+
+          lits.push_back( abc::Abc_Var2Lit( select[i][k][j], 0 ) );
+        }
+
+        if ( !abc::bmcg_sat_solver_addclause( solver, &lits[0], lits.size() ) )
+        {
+          return false;
+        }
+
+        for ( auto l = 1; l < lits.size(); ++l )
+        {
+          for ( auto m = 0; m < l; ++m )
+          {
+            int plits[] = {abc::Abc_LitNot( lits[m] ), abc::Abc_LitNot( lits[l] )};
+            if ( !abc::bmcg_sat_solver_addclause( solver, plits, 2 ) )
+            {
+              return false;
+            }
+          }
+        }
+
+        /* TODO symmetry breaking */
+      }
+    }
+
+    /* every gate needs to be used */
+    for ( auto& lits : output_lits )
+    {
+      if ( !abc::bmcg_sat_solver_addclause( solver, &lits[0], lits.size() ) )
+      {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  bool add_minterm( uint32_t minterm, int expected_value )
+  {
+    std::array<bool, NumVars> assignment;
+
+    for ( auto i = 0; i < NumVars; ++i )
+    {
+      assignment[i] = ( minterm >> i ) & 1;
+    }
+
+    abc::bmcg_sat_solver_set_nvars( solver, var_index + 4 * NumGates );
+
+    /* structure propagation */
+    for ( auto i = 0; i < NumGates; ++i )
+    {
+      const auto base_var_i = var_index + 4 * i;
+
+      for ( auto k = 0; k < 3; ++k )
+      {
+
+        for ( auto j = 0; j < NumVars + i; ++j )
+        {
+          if ( !select[i][k][j] )
+            continue;
+
+          std::vector<int> lits;
+          lits.push_back( abc::Abc_Var2Lit( select[i][k][j], 1 ) );
+
+          if ( j < NumVars ) /* node i'th k'th input should have value of PI j */
+          {
+            lits.push_back( abc::Abc_Var2Lit( base_var_i + k, assignment[j] ? 0 : 1 ) );
+            if ( !abc::bmcg_sat_solver_addclause( solver, &lits[0], lits.size() ) )
+            {
+              return false;
+            }
+          }
+          else /* node i'th k'th input should have value of node j */
+          {
+            const auto base_var_j = var_index + 4 * ( j - NumVars );
+            lits.push_back( abc::Abc_Var2Lit( base_var_j + 3, 0 ) );
+            lits.push_back( abc::Abc_Var2Lit( base_var_i + k, 1 ) );
+            if ( !abc::bmcg_sat_solver_addclause( solver, &lits[0], lits.size() ) )
+            {
+              return false;
+            }
+            lits[1] = abc::Abc_LitNot( lits[1] );
+            lits[2] = abc::Abc_LitNot( lits[2] );
+            if ( !abc::bmcg_sat_solver_addclause( solver, &lits[0], lits.size() ) )
+            {
+              return false;
+            }
+          }
+        }
+      }
+
+      for ( auto n = 0; n < 2; ++n )
+      {
+        if ( i >= NumGates - 2 && expected_value < 2 && n == expected_value )
+        {
+          continue;
+        }
+
+        for ( auto k = 0; k < 3; k++ )
+        {
+          int pLits[3], nLits = 0;
+          if ( k != 0 )
+          {
+            pLits[nLits++] = abc::Abc_Var2Lit( base_var_i + 0, n );
+          }
+          if ( k != 1 )
+          {
+            pLits[nLits++] = abc::Abc_Var2Lit( base_var_i + 1, n );
+          }
+          if ( k != 2 )
+          {
+            pLits[nLits++] = abc::Abc_Var2Lit( base_var_i + 2, n );
+          }
+          if ( i < NumGates - 2 || expected_value == 2 )
+          {
+            pLits[nLits++] = abc::Abc_Var2Lit( base_var_i + 3, !n );
+          }
+          assert( nLits <= 3 );
+          if ( !abc::bmcg_sat_solver_addclause( solver, pLits, nLits ) )
+          {
+            return false;
+          }
+        }
+      }
+    }
+
+    /* special case for expected_value == 2 */
+    if ( expected_value == 2 )
+    {
+      int pLits[2];
+      pLits[0] = abc::Abc_Var2Lit( var_index + 4 * ( NumGates - 2 ), 0 );
+      pLits[1] = abc::Abc_Var2Lit( var_index + 4 * ( NumGates - 1 ), 0 );
+      if ( !abc::bmcg_sat_solver_addclause( solver, pLits, 2 ) )
+      {
+        return false;
+      }
+      pLits[0] = abc::Abc_LitNot( pLits[0] );
+      pLits[1] = abc::Abc_LitNot( pLits[1] );
+      if ( !abc::bmcg_sat_solver_addclause( solver, pLits, 2 ) )
+      {
+        return false;
+      }
+    }
+
+    var_index += 4 * NumGates;
+
+    return true;
+  }
+
+private:
+  std::array<std::array<std::array<int, NumGates + NumVars - 1>, 3>, NumGates> select;
+  std::array<std::vector<int>, NumGates - 1> output_lits;
+
+  abc::bmcg_sat_solver* solver;
+
+  int var_index = 1;
+};
+}
+
+template<uint32_t NumVars, uint32_t NumGates>
+void xmg_exact_decomposition()
+{
+  detail::xmg_exact_decomposition_impl<NumVars, NumGates> impl;
+  if ( impl.run() )
+  {
+    impl.print_solution();
+  }
+}
+}
+
+#endif
+
+// Local Variables:
+// c-basic-offset: 2
+// eval: (c-set-offset 'substatement-open 0)
+// eval: (c-set-offset 'innamespace 0)
+// End:
