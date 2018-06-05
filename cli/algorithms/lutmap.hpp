513c5bc3d25f66673dfffe0a41493afeece935a6
Mathias Soeken <mathias.soeken@epfl.ch>
2018-06-05 12:07:24 +0200

    LUT mapping parameters.

--- cli/algorithms/lutmap.hpp
+++ cli/algorithms/lutmap.hpp
@@ -12,6 +12,8 @@ class lutmap_command : public cirkit::cirkit_command<lutmap_command, aig_t, mig_
 public:
   lutmap_command( environment::ptr& env ) : cirkit::cirkit_command<lutmap_command, aig_t, mig_t, klut_t>( env, "Performs k-LUT mapping", "apply LUT-mapping to {0}" )
   {
+    add_option( "-k,--lutsize", ps.cut_enumeration_ps.cut_size, "cut size", true );
+    add_option( "--lutcount", ps.cut_enumeration_ps.cut_limit, "number of cuts per node", true );
     add_flag( "--nofun", "do not compute cut functions" );
   }
 
@@ -20,13 +22,16 @@ public:
   {
     if ( is_set( "nofun" ) )
     {
-      mockturtle::lut_mapping( *( env->store<Store>().current() ) );
+      mockturtle::lut_mapping( *( store<Store>().current() ), ps );
     }
     else
     {
-      mockturtle::lut_mapping<typename Store::element_type, true>( *( env->store<Store>().current() ) );
+      mockturtle::lut_mapping<typename Store::element_type, true>( *( store<Store>().current() ), ps );
     }
   }
+
+private:
+  mockturtle::lut_mapping_params ps;
 };
 
 ALICE_ADD_COMMAND( lutmap, "Mapping" )
