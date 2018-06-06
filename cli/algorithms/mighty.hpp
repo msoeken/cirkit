177a8ac3f4ca5e200665de64d5f800fc000c9e15
Mathias Soeken <mathias.soeken@epfl.ch>
2018-06-06 14:30:49 +0200

    Select strategy in mighty command.

--- cli/algorithms/mighty.hpp
+++ cli/algorithms/mighty.hpp
@@ -14,6 +14,11 @@ class mighty_command : public cirkit::cirkit_command<mighty_command, mig_t>
 public:
   mighty_command( environment::ptr& env ) : cirkit::cirkit_command<mighty_command, mig_t>( env, "Performs algebraic MIG rewriting", "applies algebraic MIG rewriting to {0}" )
   {
+    opts.add_set( "--strategy", ps.strategy,
+                  {mockturtle::mig_algebraic_depth_rewriting_params::dfs,
+                   mockturtle::mig_algebraic_depth_rewriting_params::aggressive,
+                   mockturtle::mig_algebraic_depth_rewriting_params::selective},
+                  "optimization strategy", true )->set_type_name( "enum/strategy in {dfs=0, aggressive=1, selective=2}" );
   }
 
   template<class Store>
@@ -21,11 +26,14 @@ public:
   {
     auto* mig_p = static_cast<mockturtle::mig_network*>( store<Store>().current().get() );
     mockturtle::depth_view depth_mig{*mig_p};
-    mockturtle::mig_algebraic_depth_rewriting( depth_mig );
+    mockturtle::mig_algebraic_depth_rewriting( depth_mig, ps );
     *mig_p = mockturtle::cleanup_dangling( *mig_p );
   }
+
+private:
+  mockturtle::mig_algebraic_depth_rewriting_params ps;
 };
 
 ALICE_ADD_COMMAND( mighty, "Synthesis" )
 
-}
+} // namespace alice
