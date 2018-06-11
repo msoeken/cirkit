6015e9834ab22b58902e55dee45124bbbceadc89
Mathias Soeken <mathias.soeken@epfl.ch>
2018-06-11 18:59:11 +0200

    Akers resynthesis.

--- cli/algorithms/miglut.hpp
+++ cli/algorithms/miglut.hpp
@@ -1,6 +1,7 @@
 #include <alice/alice.hpp>
 
 #include <mockturtle/algorithms/node_resynthesis.hpp>
+#include <mockturtle/algorithms/node_resynthesis/akers.hpp>
 #include <mockturtle/algorithms/node_resynthesis/mig_npn.hpp>
 
 #include "../utils/cirkit_command.hpp"
@@ -13,6 +14,7 @@ class miglut_command : public cirkit::cirkit_command<miglut_command, klut_t>
 public:
   miglut_command( environment::ptr& env ) : cirkit::cirkit_command<miglut_command, klut_t>( env, "Performs LUT resynthesis", "apply LUT resynthesis to {0}" )
   {
+    add_option( "--strategy", strategy, "resynthesis strategy", true )->set_type_name( "strategy in {mignpn=0, akers=1}" );
     add_new_option();
   }
 
@@ -20,13 +22,34 @@ public:
   inline void execute_store()
   {
     const auto& ntk = *( store<Store>().current() );
-    mockturtle::mig_npn_resynthesis resyn;
+
     extend_if_new<mig_t>();
-    const auto mig = mockturtle::node_resynthesis<mockturtle::mig_network>( ntk, resyn );
+
+    const auto mig = [&]() {
+      switch ( strategy )
+      {
+      default:
+      case 0:
+      {
+        mockturtle::mig_npn_resynthesis resyn;
+        return mockturtle::node_resynthesis<mockturtle::mig_network>( ntk, resyn );
+      }
+      break;
+      case 1:
+      {
+        mockturtle::akers_resynthesis resyn;
+        return mockturtle::node_resynthesis<mockturtle::mig_network>( ntk, resyn );
+      }
+      }
+    }();
+
     store<mig_t>().current() = std::make_shared<mig_nt>( mig );
   }
+
+private:
+  unsigned strategy{0u};
 };
 
 ALICE_ADD_COMMAND( miglut, "Synthesis" )
 
-}
+} // namespace alice
