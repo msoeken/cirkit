6b95514c150550d0286e88790f4a28be36c529bd
Heinz Riener <heinz.riener@gmail.com>
2018-07-30 16:54:46 +0200

    flow.

--- cirkit.rs
+++ cirkit.rs
@@ -9,3 +9,4 @@ alias "rsz(.*)" "resub -p -z{}"
 # flows
 alias "compress2" "rw; rf; rw; rwz; rfz; rwz"
 alias "compress2rs" "rs --max_pis 6; rw; rs --max_pis 6 --depth 2; rf; rs --max_pis 8; rs --max_pis 8 --depth 2; rw; rs --max_pis 10; rwz; rs --max_pis 10 --depth 2; rs --max_pis 12; rfz; rs --max_pis 12 --depth 2; rwz"
+alias "shake" "rw; rf; rfz; rwz; rfz"
