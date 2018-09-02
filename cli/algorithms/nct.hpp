1f400e35ae7551a0af72778264f6c6aa55f696ff
Mathias Soeken <mathias.soeken@epfl.ch>
2018-09-02 16:36:01 +0200

    RevKit: nct --controls_threshold.

--- cli/algorithms/nct.hpp
+++ cli/algorithms/nct.hpp
@@ -13,6 +13,7 @@ class nct_command : public alice::command
 public:
   nct_command( const environment::ptr& env ) : command( env, "Maps MCT circuit into Quantum circuit with 2-controlled Toffoli gates" )
   {
+    add_option( "-t,--controls_threshold", ps.controls_threshold, "maximum control line threshold" );
     add_flag( "-n,--new", "adds new store entry" );
   }
 
@@ -26,13 +27,16 @@ public:
     auto& circs = store<small_mct_circuit_t>();
 
     small_mct_circuit_t circ;
-    tweedledum::nct_mapping( circ, circs.current() );
+    tweedledum::nct_mapping( circ, circs.current(), ps );
     if ( is_set( "new" ) )
     {
       circs.extend();
     }
     circs.current() = circ;
   }
+
+private:
+  tweedledum::nct_mapping_params ps;
 };
 
 ALICE_ADD_COMMAND( nct, "Mapping" );
