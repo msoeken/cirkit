fddac6d71dcf153430087ef342d60e3f81a6b21b
Mathias Soeken <mathias.soeken@epfl.ch>
2018-09-02 16:35:54 +0200

    RevKit: dbs -v.

--- cli/algorithms/dbs.hpp
+++ cli/algorithms/dbs.hpp
@@ -15,8 +15,9 @@ class dbs_command : public alice::command
 public:
   dbs_command( const environment::ptr& env ) : command( env, "Decomposition-based synthesis" )
   {
-    add_option( "--stg", strategy, "synthesis strategy", true )->set_type_name( "strategy in {pprm=0, spectrum=1, pkrm=2}" );
+    add_option( "--stg", strategy, "synthesis strategy", true )->set_type_name( "strategy in {pprm=0, spectrum=1, pkrm=2, db=3}" );
     add_flag( "-n,--new", "adds new store entry" );
+    add_flag( "-v,--verbose", "be verbose" );
   }
 
   rules validity_rules() const override
@@ -29,6 +30,9 @@ public:
     auto const& perms = store<perm_t>();
     auto f = perms.current(); // will be modified by dbs
 
+    tweedledum::decomposition_based_synthesis_params ps;
+    ps.verbose = is_set( "verbose" );
+
     if (strategy == 0u)
     {
       auto& circs = store<small_mct_circuit_t>();
@@ -37,7 +41,7 @@ public:
         circs.extend();
       }
       circs.current() = small_mct_circuit_t();
-      tweedledum::decomposition_based_synthesis( circs.current(), f, tweedledum::stg_from_pprm() );
+      tweedledum::decomposition_based_synthesis( circs.current(), f, tweedledum::stg_from_pprm(), ps );
     }
     else if (strategy == 1u)
     {
@@ -47,7 +51,7 @@ public:
         circs.extend();
       }
       circs.current() = qc_circuit_t();
-      tweedledum::decomposition_based_synthesis( circs.current(), f, tweedledum::stg_from_spectrum() );
+      tweedledum::decomposition_based_synthesis( circs.current(), f, tweedledum::stg_from_spectrum(), ps );
     }
     else if (strategy == 2u)
     {
@@ -57,7 +61,17 @@ public:
         circs.extend();
       }
       circs.current() = small_mct_circuit_t();
-      tweedledum::decomposition_based_synthesis( circs.current(), f, tweedledum::stg_from_pkrm() );
+      tweedledum::decomposition_based_synthesis( circs.current(), f, tweedledum::stg_from_pkrm(), ps );
+    }
+    else if (strategy == 3u)
+    {
+      auto& circs = store<qc_circuit_t>();
+      if ( circs.empty() || is_set( "new" ) )
+      {
+        circs.extend();
+      }
+      circs.current() = qc_circuit_t();
+      tweedledum::decomposition_based_synthesis( circs.current(), f, tweedledum::stg_from_db(), ps );
     }
   }
 
