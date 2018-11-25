627baad4a618898a7614c7ded285b85ed2d12cee
Heinz Riener <heinz.riener@gmail.com>
2018-11-25 17:19:48 +0100

    resub_command.

--- cli/algorithms/resubstitute.hpp
+++ cli/algorithms/resubstitute.hpp
@@ -8,10 +8,10 @@
 namespace alice
 {
 
-class resub_command : public cirkit::cirkit_command<resub_command, mig_t>
+class resub_command : public cirkit::cirkit_command<resub_command, aig_t, mig_t, xag_t, xmg_t>
 {
 public:
-  resub_command( environment::ptr& env ) : cirkit::cirkit_command<resub_command, mig_t>( env, "Performs resubstitution", "apply resubstitution to {0}" )
+  resub_command( environment::ptr& env ) : cirkit::cirkit_command<resub_command, aig_t, mig_t, xag_t, xmg_t>( env, "Performs resubstitution", "apply resubstitution to {0}" )
   {
     add_option( "--max_pis", ps.max_pis, "maximum number of PIs in reconvergence-driven window", true );
     add_option( "--max_nodes", ps.max_nodes, "maximum number of nodes in reconvergence-driven window", true );
@@ -26,9 +26,37 @@ public:
   template<class Store>
   inline void execute_store()
   {
-    auto* mig_p = static_cast<mockturtle::mig_network*>( store<Store>().current().get() );
-    mockturtle::resubstitution( *mig_p, ps );
-    *mig_p = cleanup_dangling( *mig_p );
+    /* aig */
+    if constexpr ( std::is_same_v<Store, aig_t> )
+    {
+      auto* aig_p = static_cast<mockturtle::aig_network*>( store<Store>().current().get() );
+      mockturtle::resubstitution( *aig_p, ps );
+      *aig_p = cleanup_dangling( *aig_p );
+    }
+
+    /* mig */
+    if constexpr ( std::is_same_v<Store, mig_t> )
+    {
+      auto* mig_p = static_cast<mockturtle::mig_network*>( store<Store>().current().get() );
+      mockturtle::resubstitution( *mig_p, ps );
+      *mig_p = cleanup_dangling( *mig_p );
+    }
+
+    /* xag */
+    if constexpr ( std::is_same_v<Store, xag_t> )
+    {
+      auto* xag_p = static_cast<mockturtle::xag_network*>( store<Store>().current().get() );
+      mockturtle::resubstitution( *xag_p, ps );
+      *xag_p = cleanup_dangling( *xag_p );
+    }
+
+    /* xmg */
+    if constexpr ( std::is_same_v<Store, xmg_t> )
+    {
+      auto* xmg_p = static_cast<mockturtle::xmg_network*>( store<Store>().current().get() );
+      mockturtle::resubstitution( *xmg_p, ps );
+      *xmg_p = cleanup_dangling( *xmg_p );
+    }
   }
 
 private:
