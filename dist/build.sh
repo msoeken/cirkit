4cd073a2973bb04b709a5edfd9bebcfda1bb45e5
Mathias Soeken <mathias.soeken@epfl.ch>
2019-04-18 15:07:28 +0200

    Remove some RevKit files.

--- dist/build.sh
+++ dist/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-CIRKIT=1
-REVKIT=1
-
 eval "$(pyenv init -)"
 
 mkdir -p wheels
@@ -10,89 +7,59 @@ mkdir -p wheels
 pyenv shell 2.7.15
 python --version
 rm -Rf cirkit/build cirkit/cirkit.egg-info cirkit/dist
-rm -Rf revkit/build revkit/revkit.egg-info revkit/dist
 python -m pip install --upgrade pip
 python -m pip install pybind11
 python -m pip install wheel
-if [ "$CIRKIT" == 1 ]; then
-  cd cirkit
-  python setup.py bdist_wheel
-  cp dist/*.whl ../wheels
-  cd ..
-fi
-if [ "$REVKIT" == 1 ]; then
-  cd revkit
-  python setup.py bdist_wheel
-  cp dist/*.whl ../wheels
-  cd ..
-fi
+
+cd cirkit
+python setup.py bdist_wheel
+cp dist/*.whl ../wheels
+cd ..
 
 pyenv shell 3.4.8
 python --version
 rm -Rf cirkit/build cirkit/cirkit.egg-info cirkit/dist
-rm -Rf revkit/build revkit/revkit.egg-info revkit/dist
 python -m pip install --upgrade pip
 python -m pip install pybind11
 python -m pip install wheel
-if [ "$CIRKIT" == 1 ]; then
-  cd cirkit
-  python setup.py bdist_wheel
-  cp dist/*.whl ../wheels
-  cd ..
-fi
-if [ "$REVKIT" == 1 ]; then
-  cd revkit
-  python setup.py bdist_wheel
-  cp dist/*.whl ../wheels
-  cd ..
-fi
+
+cd cirkit
+python setup.py bdist_wheel
+cp dist/*.whl ../wheels
+cd ..
 
 pyenv shell 3.5.5
 python --version
 rm -Rf cirkit/build cirkit/cirkit.egg-info cirkit/dist
-rm -Rf revkit/build revkit/revkit.egg-info revkit/dist
 python -m pip install --upgrade pip
 python -m pip install pybind11
 python -m pip install wheel
+
 cd cirkit
 python setup.py bdist_wheel
+cp dist/*.whl ../wheels
+cd ..
 
 pyenv shell 3.6.5
 python --version
 rm -Rf cirkit/build cirkit/cirkit.egg-info cirkit/dist
-rm -Rf revkit/build revkit/revkit.egg-info revkit/dist
 python -m pip install --upgrade pip
 python -m pip install pybind11
 python -m pip install wheel
-if [ "$CIRKIT" == 1 ]; then
-  cd cirkit
-  python setup.py bdist_wheel
-  cp dist/*.whl ../wheels
-  cd ..
-fi
-if [ "$REVKIT" == 1 ]; then
-  cd revkit
-  python setup.py bdist_wheel
-  cp dist/*.whl ../wheels
-  cd ..
-fi
+
+cd cirkit
+python setup.py bdist_wheel
+cp dist/*.whl ../wheels
+cd ..
 
 pyenv shell 3.7.0
 python --version
 rm -Rf cirkit/build cirkit/cirkit.egg-info cirkit/dist
-rm -Rf revkit/build revkit/revkit.egg-info revkit/dist
 python -m pip install --upgrade pip
 python -m pip install pybind11
 python -m pip install wheel
-if [ "$CIRKIT" == 1 ]; then
-  cd cirkit
-  python setup.py bdist_wheel
-  cp dist/*.whl ../wheels
-  cd ..
-fi
-if [ "$REVKIT" == 1 ]; then
-  cd revkit
-  python setup.py bdist_wheel
-  cp dist/*.whl ../wheels
-  cd ..
-fi
+
+cd cirkit
+python setup.py bdist_wheel
+cp dist/*.whl ../wheels
+cd ..
